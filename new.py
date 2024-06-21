0cab70555e07dfa443306610e4273fe0d2e23a4a
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-06-21 14:41:19 +0530

    Create new.py

---
+++ new.py
@@ -0,0 +1,56 @@
+"""
+Install the Google AI Python SDK
+
+$ pip install google-generativeai
+
+See the getting started guide for more information:
+https://ai.google.dev/gemini-api/docs/get-started/python
+"""
+
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()
+
+google_API = os.getenv("google_API")
+
+genai.configure(api_key = google_API)
+
+# Create the model
+# See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
+generation_config = {
+  "temperature": 0,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
+
+chat_session = model.start_chat(
+  history=[
+    {
+      "role": "user",
+      "parts": [
+        "hi\n",
+      ],
+    },
+    {
+      "role": "model",
+      "parts": [
+        "Hi there! What can I do for you today? \n",
+      ],
+    },
+  ]
+)
+
+response = chat_session.send_message("INSERT_INPUT_HERE")
+
+print(response.text)
